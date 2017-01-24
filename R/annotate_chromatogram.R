8708ce8646e3c82537f2a0c89c8e8f5fe7870d54
Cihan Erkut <cihan.erkut@embl.de>
2017-01-24 21:40:49 +0100

    annotate chromatograms

--- R/annotate_chromatogram.R
+++ R/annotate_chromatogram.R
@@ -16,21 +16,43 @@
 #' as warning.
 #' 
 #' @note This function may overwrite some of the annotation from chromatogram
-#' data if \code{sample_names} was already used during import via \code{read_unicorn}.
-#' all other columns are be added accordingly.
+#' data if \code{sample_names} was already used during import via 
+#' \code{read_unicorn}. All other columns are be added accordingly.
 #' 
 #' @param chr_data Chromatogram data imported via \code{read_unicorn}
 #' @param sample_info Sample information data frame.
-
+#' @param Sample Obligatory \code{Sample} column for the sample information data
+#' frame.
+#' @param ... Additional columns for the sample information data frame.
+#' 
+#' @examples
+#' \dontrun{
+#' chr_data <- read_unicorn("Chromatogram.xls")
+#' 
+#' list_IDs(chr_data)
+#' #                ID
+#' # 1 Manual run 10:1
+#' # 2 Manual run 10:5
+#' # 3 Manual run 10:3
+#' 
+#' sample_info <- create_annotation(
+#'     chr_data,
+#'     Sample = c("Negative control", "Positive control", "Test"),
+#'     Run = c(1, 2, 3))
+#'   
+#' annotate_chromatogram(chr_data, sample_info)
+#' }
+#' 
 #' @family data annotation functions
+#' 
 #' @seealso \code{\link{read_unicorn}}
-#' @importFrom magrittr "%<>%" "%>%"
+#' 
+#' @importFrom magrittr "%<>%" "%>%" set_colnames
+#' @importFrom dplyr left_join select
 #' @import assertthat
-#' @import dplyr
 #' @export 
 
 # TODO: Write a function to import sample_info from a CSV file.
-# TODO: Write an addin to create the CSV file interactively.
 
 annotate_chromatogram <- function(chr_data, sample_info) {
   assert_that(is.data.frame(sample_info),
@@ -47,9 +69,29 @@ annotate_chromatogram <- function(chr_data, sample_info) {
 #' @rdname annotate_chromatogram
 #' @export
 list_IDs <- function(chr_data) {
-  IDs <- chr_data$ID %>%
+  IDs <- chr_data %>%
+    select(ID) %>%
     unique %>%
     unlist(use.names = FALSE) %>%
-    data.frame(IDs = .)
-  print(IDs)
+    data.frame(ID = .)
+  return(IDs)
+}
+
+#' @rdname annotate_chromatogram
+#' @export
+create_annotation <- function(chr_data, Sample = NULL, ...) {
+  ID <- list_IDs(chr_data)
+  n = nrow(ID)
+  n_groups <- sapply(list(...), length)
+  
+  if (is.null(Sample)) {
+    Sample <- list_IDs(chr_data) %>%
+      set_colnames('Sample')
+  } else {
+    assert_that(is.vector(Sample), are_equal(length(Sample), n))
+  }
+  assert_that(all(n_groups == n))
+  
+  annotation_df <- data.frame(ID, Sample, ...)
+  return(annotation_df)
 }
